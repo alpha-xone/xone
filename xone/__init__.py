d9e23ad7dba452f4213ab3ccd8dfeb0a3acc0871
alpha-xone <alpha.xone@outlook.com>
2018-07-19 02:25:28 +0800

    add doc test for module

--- xone/__init__.py
+++ xone/__init__.py
@@ -1,1 +1,7 @@
-__version__ = '0.0.2'
+__version__ = '0.0.3'
+
+__submodules__ = [
+    'utils',
+    'calendar',
+    'files',
+]
---
+++ xone/__main__.py
@@ -0,0 +1,7 @@
+def main():
+    import xdoctest as xdoc
+    xdoc.doctest_module('xone')
+
+
+if __name__ == '__main__':
+    main()
