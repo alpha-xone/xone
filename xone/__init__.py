cfd8553901ddc38b3d9742ce22c7a44b75bf3adc
alpha-xone <alpha.xone@outlook.com>
2018-07-19 22:29:32 +0800

    revert mkinit project

--- xone/__init__.py
+++ xone/__init__.py
@@ -7,23 +7,3 @@ __submodules__ = [
     'procs',
     'plots',
 ]
-from xone import utils
-from xone import calendar
-from xone import files
-from xone import procs
-from xone import plots
-
-from xone.utils import (align_data, cur_time, flatten, fmt_dt, latest_trading,
-                        spline_curve, tolist,)
-from xone.calendar import (USTradingCalendar, trading_dates,)
-from xone.files import (DATE_FMT, all_files, all_folders, create_folder,
-                        file_modified_time, latest_file,)
-from xone.procs import (run, saturate_kwargs,)
-from xone.plots import (plot_h, plot_multi,)
-
-__all__ = ['DATE_FMT', 'USTradingCalendar', 'align_data', 'all_files',
-           'all_folders', 'calendar', 'create_folder', 'cur_time',
-           'file_modified_time', 'files', 'flatten', 'fmt_dt', 'latest_file',
-           'latest_trading', 'plot_h', 'plot_multi', 'plots', 'procs', 'run',
-           'saturate_kwargs', 'spline_curve', 'tolist', 'trading_dates',
-           'utils']
