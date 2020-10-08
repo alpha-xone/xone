40ac2f0ca3c396edf8b2bb1d28c631eb4ea34502
alpha-xone <alpha.xone@outlook.com>
2020-10-08 15:03:37 +0800

    decorator for caching local files

--- xone/cache.py
+++ xone/cache.py
@@ -1,142 +1,146 @@
-import hashlib
-import json
-
 import pandas as pd
 
+import os
 import sys
 import inspect
+
 from functools import wraps
+from parse import parse, compile
 from xone import utils, files, logs
 
+LOAD_FUNC = {
+    'pkl': pd.read_pickle,
+    'parq': pd.read_parquet,
+    'csv': pd.read_csv,
+    'xls': pd.read_excel,
+    'xlsx': pd.read_excel,
+}
 
-def cache_file(symbol, func, has_date, root, date_type='date'):
+SAVE_FUNC = {
+    'pkl': 'to_pickle',
+    'parq': 'to_parquet',
+    'csv': 'to_csv',
+    'xlsx': 'to_excel',
+    'xls': 'to_excel',
+}
+
+
+def with_cache(*dec_args, **dec_kwargs):
     """
-    Data file
+    Wraps function to load cache data if available
+    """
+    # Data root path
+    data_root = dec_kwargs.get('data_path', None)
+    # File format
+    file_fmt = dec_kwargs.get('file_fmt', None)
+    # Update frequency - in pd.Timedelta - determines how frequent data should be updated
+    update_freq = dec_kwargs.get('update_freq', None)
+
+    def decorator(func):
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+
+            # Check function parameters
+            param = inspect.signature(func).parameters
+            all_kw = {
+                k: args[n] if n < len(args) else v.default
+                for n, (k, v) in enumerate(param.items())
+            }
+            param_kind = pd.Series({k: v.kind for k, v in param.items()})
+            if param_kind.max() == 4:
+                all_kw.update(kwargs)
+            else:
+                all_kw.update(**{k: v for k, v in kwargs.items() if k in param_kind})
+            kwargs.update(all_kw)
+
+            # Data path and file name
+            cur_dt = utils.cur_time(tz=kwargs.get('tz', 'UTC'))
+            root_path = getattr(sys.modules[func.__module__], 'DATA_PATH') \
+                if not data_root else data_root
+            file_name = target_file_name(fmt=file_fmt, **all_kw) \
+                if file_fmt else f'{func.__name__}/[date].pkl'
+
+            data_file = (
+                f'{root_path}/{file_name}'
+                .replace('\\', '/')
+                .replace('[today]', cur_dt)
+                .replace('[date]', cur_dt)
+            )
+
+            # Load data if exists
+            if files.exists(data_file):
+                return load_file(data_file=data_file, **kwargs)
+            elif update_freq:
+                pattern = compile(data_file.replace('[', '{').replace(']', '}'))
+                cache_files = sorted(
+                    filter(
+                        lambda _: pattern.parse(_),
+                        files.all_files('/'.join(data_file.split('/')[:-1]))
+                    ),
+                    key=os.path.getmtime,
+                    reverse=True,
+                )
+                if len(cache_files) > 0:
+                    latest = cache_files[0]
+                    if pd.Timestamp('now') - files.file_modified_time(latest) < \
+                            pd.Timedelta(update_freq):
+                        return load_file(data_file=latest, **kwargs)
+
+            # Retrieve data
+            data = func(**all_kw)
+            # Save data to cache
+            save_file(data=data, data_file=data_file, **kwargs)
+
+            return data
+        return wrapper
+
+    return decorator(dec_args[0]) if dec_args and callable(dec_args[0]) else decorator
+
+
+def target_file_name(fmt: str, **kwargs) -> str:
+    """
+    Target file name
 
     Args:
-        symbol: symbol
-        func: use function to categorize data
-        has_date: contains date in data file
-        root: root path
-        date_type: parameters pass to utils.cur_time, [date, time, time_path, ...]
+        fmt: f-string format
 
     Returns:
-        str: date file
-    """
-    cur_mod = sys.modules[func.__module__]
-    data_tz = getattr(cur_mod, 'DATA_TZ') if hasattr(cur_mod, 'DATA_TZ') else 'UTC'
-    cur_dt = utils.cur_time(typ=date_type, tz=data_tz, trading=False)
-
-    if has_date:
-        file_fmt = getattr(cur_mod, 'FILE_WITH_DATE', '{root}/{typ}/{symbol}/{cur_dt}.parq')
-    else:
-        file_fmt = getattr(cur_mod, 'FILE_NO_DATE', '{root}/{typ}/{symbol}.parq')
-
-    return data_file(
-        file_fmt=file_fmt, root=root, cur_dt=cur_dt, typ=func.__name__, symbol=symbol
-    )
-
-
-def update_data(func):
-    """
-    Decorator to save data more easily. Use parquet as data format
-
-    Args:
-        func: function to load data from data source
-
-    Returns:
-        wrapped function
-    """
-    default = dict([
-        (param.name, param.default)
-        for param in inspect.signature(func).parameters.values()
-        if param.default != getattr(inspect, '_empty')
-    ])
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-
-        default.update(kwargs)
-        kwargs.update(default)
-        cur_mod = sys.modules[func.__module__]
-        logger = logs.get_logger(name_or_func=f'{cur_mod.__name__}.{func.__name__}', types='stream')
-
-        root_path = getattr(cur_mod, 'DATA_PATH')
-        date_type = kwargs.pop('date_type', 'date')
-        save_static = kwargs.pop('save_static', True)
-        save_dynamic = kwargs.pop('save_dynamic', True)
-        symbol = kwargs.get('symbol')
-        file_kw = dict(func=func, symbol=symbol, root=root_path, date_type=date_type)
-        d_file = cache_file(has_date=True, **file_kw)
-        s_file = cache_file(has_date=False, **file_kw)
-
-        cached = kwargs.pop('cached', False)
-        if cached and save_static and files.exists(s_file):
-            logger.info(f'Reading data from {s_file} ...')
-            return pd.read_parquet(s_file)
-
-        data = func(*args, **kwargs)
-
-        if save_static:
-            files.create_folder(s_file, is_file=True)
-            save_data(data=data, file_fmt=s_file, append=False)
-            logger.info(f'Saved data file to {s_file} ...')
-
-        if save_dynamic:
-            drop_dups = kwargs.pop('drop_dups', None)
-            files.create_folder(d_file, is_file=True)
-            save_data(data=data, file_fmt=d_file, append=True, drop_dups=drop_dups)
-            logger.info(f'Saved data file to {d_file} ...')
-
-        return data
-
-    return wrapper
-
-
-def save_data(data, file_fmt, append=False, drop_dups=None, info=None, **kwargs):
-    """
-    Save data to file
-
-    Args:
-        data: pd.DataFrame
-        file_fmt: data file format in terms of f-strings
-        append: if append data to existing data
-        drop_dups: list, drop duplicates in columns
-        info: dict, infomation to be hashed and passed to f-strings
-        **kwargs: additional parameters for f-strings
+        str
 
     Examples:
-        >>> # sample = pd.DataFrame([[1, 2], [3, 4]], columns=['a', 'b'])
-        >>> # save_data(
-        >>>     # sample, '{ROOT}/daily/{typ}.parq',
-        >>>     # ROOT='tests/data', typ='earnings'
-        >>> # )
+        >>> target_file_name('data/ticker={ticker}.pkl', ticker='RDS/A')
+        'data/ticker=RDS_A.pkl'
     """
-    d_file = data_file(file_fmt=file_fmt, info=info, **kwargs)
-    if append and files.exists(d_file):
-        data = pd.DataFrame(pd.concat([pd.read_parquet(d_file), data], sort=False))
-        if drop_dups is not None:
-            data.drop_duplicates(subset=utils.tolist(drop_dups), inplace=True)
-
-    if not data.empty: data.to_parquet(d_file)
-    return data
+    return utils.fstr(fmt=fmt, **{k: v.replace('/', '_') for k, v in kwargs.items()})
 
 
-def data_file(file_fmt, info=None, **kwargs):
+def load_file(data_file: str, **kwargs):
     """
-    Data file name for given infomation
-
-    Args:
-        file_fmt: file format in terms of f-strings
-        info: dict, to be hashed and then pass to f-string using 'hash_key'
-              these info will also be passed to f-strings
-        **kwargs: arguments for f-strings
-
-    Returns:
-        str: data file name
+    Load data from cache
     """
-    if isinstance(info, dict):
-        kwargs['hash_key'] = hashlib.sha256(json.dumps(info).encode('utf-8')).hexdigest()
-        kwargs.update(info)
+    logger = logs.get_logger(load_file, level=kwargs.get('log', 'info'))
+    if (not data_file) or (not files.exists(data_file)): return
 
-    return utils.fstr(fmt=file_fmt, **kwargs)
+    ext = data_file.split('.')[-1]
+    if ext not in LOAD_FUNC: return
+
+    logger.debug(f'Reading from {data_file} ...')
+    return LOAD_FUNC[ext](data_file)
+
+
+def save_file(data, data_file: str, **kwargs):
+    """
+    Save data
+    """
+    logger = logs.get_logger(save_file, level=kwargs.pop('log', 'info'))
+    if not data_file: return
+    ext = data_file.split('.')[-1]
+    if ext in ['csv', 'xls', 'xlsx']: kwargs['index'] = False
+
+    save_func = SAVE_FUNC.get(ext, '__nothing__')
+    if not hasattr(data, save_func): return
+
+    files.create_folder(data_file, is_file=True)
+    logger.debug(f'Saving data to {data_file} ...')
+    getattr(data, save_func)(data_file, **kwargs)
