51fcf64c59622611abc9c4a2355578aa9f3b6f52
alpha-xone <alpha.xone@outlook.com>
2021-01-02 02:19:03 +0800

    replace os.path with pathlib

--- xone/files.py
+++ xone/files.py
@@ -1,8 +1,7 @@
-import pandas as pd
-
 import os
-import glob
 import time
+
+from pathlib import Path
 
 # Default datetime format:
 #   ISO 8601 format date/time
@@ -26,7 +25,7 @@ def exists(path) -> bool:
     return os.path.exists(path=path)
 
 
-def abspath(cur_file, parent=0) -> str:
+def abspath(cur_file, parent=0) -> Path:
     """
     Absolute path
 
@@ -37,10 +36,10 @@ def abspath(cur_file, parent=0) -> str:
     Returns:
         str
     """
-    file_path = os.path.abspath(cur_file).replace('\\', '/')
-    if os.path.isdir(file_path) and parent == 0: return file_path
-    adj = 1 - os.path.isdir(file_path)
-    return '/'.join(file_path.split('/')[:-(parent + adj)])
+    p = Path(cur_file)
+    cur_path = p.parent if p.is_file() else p
+    if parent == 0: return cur_path
+    return abspath(cur_file=cur_path.parent, parent=parent - 1)
 
 
 def create_folder(path_name: str, is_file=False):
@@ -51,11 +50,8 @@ def create_folder(path_name: str, is_file=False):
         path_name: full path name
         is_file: whether input is name of file
     """
-    path_sep = path_name.replace('\\', '/').split('/')
-    for i in range(1, len(path_sep) + (0 if is_file else 1)):
-        cur_path = '/'.join(path_sep[:i])
-        if not cur_path: continue
-        if not os.path.exists(cur_path): os.mkdir(cur_path)
+    p = Path(path_name).parent if is_file else Path(path_name)
+    p.mkdir(parents=True, exist_ok=True)
 
 
 def all_files(
@@ -78,33 +74,21 @@ def all_files(
         list: all file names with criteria fulfilled
 
     Examples:
-        >>> test_folder = f'{abspath(__file__)}/tests/files'
+        >>> test_folder = abspath(__file__) / 'tests/files'
         >>> all_files(test_folder, keyword='test', full_path=False)
         ['test_1.json', 'test_2.json']
         >>> all_files(test_folder, has_date=True, full_path=False)
         ['dates_2019-01-01.yml', 'dates_2019-01-02.yml']
     """
-    if not os.path.exists(path=path_name): return []
-    path_name = path_name.replace('\\', '/')
+    p = Path(path_name)
+    if not p.is_dir(): return []
 
-    if keyword or ext:
-        keyword = f'*{keyword}*' if keyword else '*'
-        if not ext: ext = '*'
-        files = sorted([
-            f.replace('\\', '/') for f in glob.iglob(f'{path_name}/{keyword}.{ext}')
-            if os.path.isfile(f) and (f.replace('\\', '/').split('/')[-1][0] != '~')
-        ])
+    keyword = f'*{keyword}*' if keyword else '*'
+    keyword += f'.{ext}' if ext else '.*'
+    files = list(filter(lambda v: v.name[0] != '~', p.glob(keyword)))
 
-    else:
-        files = sorted([
-            f'{path_name}/{f}' for f in os.listdir(path=path_name)
-            if os.path.isfile(f'{path_name}/{f}') and (f[0] != '~')
-        ])
-
-    if has_date:
-        files = filter_by_dates(files, date_fmt=date_fmt)
-
-    return files if full_path else [f.split('/')[-1] for f in files]
+    if has_date: files = filter_by_dates(files, date_fmt=date_fmt)
+    return files if full_path else [f.name for f in files]
 
 
 def all_folders(
@@ -124,35 +108,26 @@ def all_folders(
         list: all folder names fulfilled criteria
 
     Examples:
-        >>> target_folder = f'{abspath(__file__)}/tests/folders'
+        >>> target_folder = abspath(__file__) / 'tests/folders'
         >>> for fld in sorted(all_folders(target_folder, keyword='test')):
-        ...     print(fld.split('/')[-1])
+        ...     print(fld.name)
         test_1
         test_2
         >>> for fld in sorted(all_folders(target_folder, has_date=True)):
-        ...     print(fld.split('/')[-1])
+        ...     print(fld.name)
         dates_2019-01-01
         dates_2019-01-02_labeled
         dates_2019-01-03
     """
-    if not os.path.exists(path=path_name): return []
-    path_name = path_name.replace('\\', '/')
+    p = Path(path_name)
+    if not p.is_dir(): return []
 
-    if keyword:
-        folders = sorted([
-            f.replace('\\', '/') for f in glob.iglob(f'{path_name}/*{keyword}*')
-            if os.path.isdir(f) and (f.replace('\\', '/').split('/')[-1][0] != '~')
-        ])
+    folders = list(filter(
+        lambda v: v.is_dir() and v.name[0] != '~',
+        p.glob(f'*{keyword}*' if keyword else '*'),
+    ))
 
-    else:
-        folders = sorted([
-            f'{path_name}/{f}' for f in os.listdir(path=path_name)
-            if os.path.isdir(f'{path_name}/{f}') and (f[0] != '~')
-        ])
-
-    if has_date:
-        folders = filter_by_dates(folders, date_fmt=date_fmt)
-
+    if has_date: folders = filter_by_dates(folders, date_fmt=date_fmt)
     return folders
 
 
@@ -189,10 +164,7 @@ def filter_by_dates(files_or_folders: list, date_fmt=DATE_FMT) -> list:
     from parse import compile
 
     p = compile(date_fmt)
-    return list(filter(
-        lambda _: p.search(str(_).replace('\\', '/').split('/')[-1]),
-        files_or_folders
-    ))
+    return list(filter(lambda _: p.search(Path(_).name), files_or_folders))
 
 
 def latest_file(path_name, keyword='', ext='', **kwargs) -> str:
@@ -208,14 +180,14 @@ def latest_file(path_name, keyword='', ext='', **kwargs) -> str:
         str: latest file name
 
     Examples:
-        >>> target_folder = f'{abspath(__file__)}/tests/folders'
+        >>> target_folder = abspath(__file__) / 'tests/folders'
         >>> latest_file(target_folder, keyword='test', ext='yml').split('/')[-1]
         'test_2.yml'
-        >>> latest_file(f'{target_folder}/notfound')
+        >>> latest_file(target_folder / 'notfound')
         ''
     """
-    files = all_files(
-        path_name=path_name, keyword=keyword, ext=ext, full_path=True
+    files = sort_by_modified(
+        all_files(path_name=path_name, keyword=keyword, ext=ext, full_path=True)
     )
 
     if not files:
@@ -225,13 +197,10 @@ def latest_file(path_name, keyword='', ext='', **kwargs) -> str:
         logger.debug(f'file is not found in folder: {path_name}')
         return ''
 
-    modified_time = [os.path.getmtime(f) for f in files]
-    files = [f for (dt, f) in sorted(zip(modified_time, files))]
-
-    return files[-1]
+    return str(files[0]).replace('\\', '/')
 
 
-def file_modified_time(file_name) -> pd.Timestamp:
+def file_modified_time(file_name):
     """
     File modified time in python
 
@@ -241,4 +210,11 @@ def file_modified_time(file_name) -> pd.Timestamp:
     Returns:
         pd.Timestamp
     """
+    import pandas as pd
+
     return pd.Timestamp(pd.to_datetime(time.ctime(os.path.getmtime(file_name))))
+
+
+if __name__ == '__main__':
+
+    print(abspath(__file__) / 'tests/files')
